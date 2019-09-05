d6e8421e572a16d73ab7c027ef69020f925c907c
JirayuK58 <54941812+JirayuK58@users.noreply.github.com>
2019-09-05 20:07:30 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,7 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = '8hAAIWJ8Wnpcmop4cs7iPwyqdQ+3mia6FpDZyxhW7sB24ZWugGr15/y5j62RXXzm9iZsq7Ni5gJnFTmAAHCoQwMI/TfjcK3AMRIprusOhwpSWPRhqrW7uFWMl4VbFhIAbypuignpOCF3jwmQQiOZ6QdB04t89/1O/w1cDnyilFU='; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => '0745c770c777185664c62920665c6e8b',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
